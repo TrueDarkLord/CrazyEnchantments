9a02b2524643b9aa53825515949b4e1a0cc3ffa1
TrueDarkLord <TrueDarkLord.email@gmail.com>
2023-04-09 15:05:00 -0600

    ArmorEquipEvent Usage Removal
    Removed the last parts that were used for the event. Only the event class file itself is left.

--- platforms/paper/src/main/java/com/badbones69/crazyenchantments/listeners/ArmorListener.java
+++ platforms/paper/src/main/java/com/badbones69/crazyenchantments/listeners/ArmorListener.java
@@ -1,22 +1,9 @@
 package com.badbones69.crazyenchantments.listeners;
 
-import com.badbones69.crazyenchantments.CrazyEnchantments;
-import com.badbones69.crazyenchantments.api.enums.ArmorType;
-import com.badbones69.crazyenchantments.api.events.ArmorEquipEvent;
-import com.badbones69.crazyenchantments.api.events.ArmorEquipEvent.EquipMethod;
-import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 /**
  * @author Arnah
  * @since Jul 30, 2015
  */
-public class ArmorListener implements Listener {
-    private final CrazyEnchantments plugin = CrazyEnchantments.getPlugin();
-    @EventHandler(ignoreCancelled = true)
-    public void paperArmorChange(PlayerArmorChangeEvent event) {
-        ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent(event.getPlayer(), EquipMethod.HOTBAR, ArmorType.matchType(event.getNewItem()), event.getOldItem(), event.getNewItem());
-        plugin.getServer().getPluginManager().callEvent(armorEquipEvent);
-    }
-}
+public class ArmorListener implements Listener {}
